9cdb9ca86d8b4d775031ced9f10b860b33ba23e3
Yuvraj dewangan <yd00102@gmail.com>
2025-10-20 15:45:58 +0530

    fix: fixing a same text

--- src/features/auth/_components/sign-in-form.tsx
+++ src/features/auth/_components/sign-in-form.tsx
@@ -177,7 +177,7 @@ export const SignInForm = ({
                                         d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.44 9.8 8.21 11.39.6.11.82-.26.82-.58 0-.29-.01-1.06-.02-2.08-3.34.73-4.05-1.61-4.05-1.61-.55-1.39-1.33-1.76-1.33-1.76-1.09-.75.08-.73.08-.73 1.2.08 1.84 1.24 1.84 1.24 1.07 1.84 2.81 1.3 3.5 1 .11-.78.42-1.3.76-1.6-2.67-.3-5.47-1.33-5.47-5.93 0-1.31.47-2.38 1.24-3.22-.12-.3-.54-1.53.12-3.18 0 0 1.01-.32 3.3 1.23.95-.27 1.97-.4 3-.4s2.05.13 3 .4c2.29-1.55 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.77.84 1.24 1.91 1.24 3.22 0 4.6-2.8 5.62-5.48 5.92.43.38.81 1.1.81 2.22 0 1.61-.02 2.92-.02 3.31 0 .32.21.7.82.58A12 12 0 0024 12c0-6.63-5.37-12-12-12z"
                                     />{" "}
                                 </svg>
-                                <span className="text-sm">Login with Github</span>
+                                <span className="text-sm">Sign in with Github</span>
                             </Button>
                             <Button
                                 variant="outline"
@@ -191,7 +191,7 @@ export const SignInForm = ({
                                         fill="currentColor"
                                     />
                                 </svg>
-                                <span className="text-sm">Login with Google</span>
+                                <span className="text-sm">Sign in with Google</span>
                             </Button>
                         </div>
                         <div className="text-center text-sm">
