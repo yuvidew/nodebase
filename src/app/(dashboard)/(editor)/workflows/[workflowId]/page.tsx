0667cbdc67d3b8512194b694a7b69ed6d9e2a9be
Yuvraj dewangan <yd00102@gmail.com>
2025-10-23 16:21:12 +0530

    feat: creating a workflow page header

--- src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -1,18 +1,39 @@
+import { Editor, EditorError, EditorLoading } from "@/features/editor/_components/editor";
+import { EditorHeader } from "@/features/editor/_components/editor-header";
+import { prefetchWorkflow } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 
 interface Props {
-    params : Promise<{
-        workflowId : string
+    /**
+     * @params Promise resolving to the workflow route params.
+     */
+    params: Promise<{
+        workflowId: string
     }>
 }
 
-const Page = async ({params} : Props) => {
+const Page = async ({ params }: Props) => {
     await requireAuth();
-    const {workflowId} = await params;
+
+    const { workflowId } = await params;
+
+    prefetchWorkflow(workflowId);
 
     return (
-        <div>Page : {workflowId}</div>
+        <HydrateClient>
+            <ErrorBoundary fallback={<EditorError/>}>
+                <Suspense fallback={<EditorLoading/>}>
+                    <EditorHeader workflowId={workflowId}/>
+                    <main className=" flex-1">
+                        <Editor workflowId={workflowId} />
+                    </main>
+                </Suspense>
+            </ErrorBoundary>
+        </HydrateClient>
     )
 }
 
